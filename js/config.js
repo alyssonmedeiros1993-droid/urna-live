be8a96f81537ac2c15cb58ea26558f707971df4e
alyssonmedeiros1993-droid <alyssonmedeiros1993@gmail.com>
2026-07-31 04:26:53 +0800

    Create config.js
    6

---
+++ js/config.js
@@ -0,0 +1,25 @@
+// ===============================
+// PROJETO URNA LIVE
+// config.js
+// ===============================
+
+const CONFIG = {
+
+    nomeProjeto: "Projeto Urna Live",
+
+    candidatos: [
+        {
+            id: 1,
+            nome: "Candidato 1",
+            foto: "https://placehold.co/400x400?text=Candidato+1"
+        },
+        {
+            id: 2,
+            nome: "Candidato 2",
+            foto: "https://placehold.co/400x400?text=Candidato+2"
+        }
+    ],
+
+    atualizacao: 3000
+
+};
